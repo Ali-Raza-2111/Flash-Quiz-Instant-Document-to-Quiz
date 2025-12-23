5964d228458c1880cf84842bb23adf0fe4195f8e
Ali Raza <aliraza21112005@gmail.com>
2025-12-23 15:21:40 +0500

    Corrected The Paths of Imports

--- Frontend/QuizUI/src/App.jsx
+++ Frontend/QuizUI/src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
-import LandingPage from './pages/landingPage'
-import LoginPage from './pages/Auth/loginPage'
-import Dashboard from './pages/dashboard'
-import QuizPage from './pages/quizPage'
-import ResultPage from './pages/resultPage'
+import LandingPage from './pages/landing/landingPage'
+import LoginPage from './pages/Auth/login/loginPage'
+import Dashboard from './pages/dashboard/dashboard'
+import QuizPage from './pages/dashboard/quizPage'
+import ResultPage from './pages/dashboard/resultPage'
 
 function App() {
   return (
